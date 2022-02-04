709cc62e90461bc74f8e6252d8355f4794068202
nullbyto <nullbyto@gmail.com>
2022-02-04 19:31:11 +0100

    added box-only + title

--- src/main.rs
+++ src/main.rs
@@ -22,15 +22,19 @@ struct Cli {
     #[structopt(short, long, conflicts_with("file"), required_unless("file"))]
     comment: Option<String>,
 
-    /// Path to file or directory to commentize in the header
+    /// Title to add on top of the box
+    #[structopt(short, long)]
+    title: Option<String>,
+
+    /// Path to file or directory to commentize in header/footer
     #[structopt(parse(from_os_str))]
     path: Option<PathBuf>,
 
-    /// Symbol to commnetize with
+    /// Symbol to commentize with
     #[structopt(short, long)]
     symbol: Option<String>,
 
-    /// Wall symbol to commnetize with
+    /// Wall symbol to commentize with
     #[structopt(short, long)]
     wall: Option<String>,
 
@@ -50,8 +54,12 @@ struct Cli {
     #[structopt(short, long)]
     right: Option<usize>,
 
-    /// Output the comment
-    #[structopt(short, long)]
+    /// Move box from beginning of line
+    #[structopt(short, long("move"), requires("box-only"))]
+    mv: Option<usize>,
+
+    /// Output result when commenting files
+    #[structopt(short, long, requires("path"))]
     output: bool,
 
     /// Append to file path
@@ -59,14 +67,16 @@ struct Cli {
     append: bool,
 
     /// Modded style
-    #[structopt(short, long, conflicts_with("symbol"), conflicts_with("wall"))]
+    #[structopt(long, conflicts_with("symbol"), conflicts_with("wall"), conflicts_with("box_only"))]
     modded: bool,
+
+    /// Comment box only (without comment delimiter)
+    #[structopt(short, long, conflicts_with("path"))]
+    box_only: bool,
 }
 
 fn main() {
     let cli = Cli::from_args();
-
-    // return println!("{:?}", cli);
 
     let mut comment = String::new();
     if let Some(c) = cli.comment {
@@ -97,22 +107,37 @@ fn main() {
         None => String::from("*"),
     };
 
-    // Modded option
-    let mut modded = String::new();
-    if cli.modded {
-        sym = String::from("///");
-        commentized.push_str("/");
-        modded.push_str("");
-    } else {
-        commentized.push_str("/*");
-        modded.push_str(" ");
-    }
+    // Title to add on top of the box
+    let title = match cli.title {
+        Some(s) => s,
+        None => String::from(""),
+    };
+
+    let title_len = title.chars().count();
 
     // Extract first char from symbol
     let one_sym = sym.chars().collect::<Vec<char>>()[0];
 
     if let Some(s) = cli.wall {
         sym = String::from(s);
+    }
+
+    let delimiter;
+
+    // Modded option
+    let mut modded = String::new();
+    if cli.modded {
+        sym = String::from("///");
+        // commentized.push_str("/");
+        delimiter = "/".to_string();
+        modded.push_str("");
+    } else if cli.box_only {
+        // commentized.push_str(&sym);
+        delimiter = one_sym.to_string();
+    } else {
+        // commentized.push_str("/*");
+        delimiter = "/*".to_string();
+        modded.push_str(" ");
     }
 
     // Width padding length
@@ -139,16 +164,27 @@ fn main() {
         None => 0,
     };
 
+    // Move box
+    let mv = match cli.mv {
+        Some(n) => n,
+        None => 0,
+    };
+
     // Take the lines of file if put
     let lines = comment
         .lines()
         .map(|s| s.to_string())
         .collect::<Vec<String>>();
     // Extract longest line length
-    let longest = match lines.iter().max_by(|x, y| x.len().cmp(&y.len())) {
-        Some(s) => s.len(),
+    // chars().count() is used for chars that take more than 1 byte
+    let mut longest = match lines.iter().max_by(|x, y| x.chars().count().cmp(&y.chars().count())) {
+        Some(s) => s.chars().count(),
         None => return println!("Error!: The file provided is empty!"),
     };
+
+    if title_len > longest {
+        longest = title_len;
+    }
 
     // Result square comment width and height
     let width = longest + (w_pad_len * 2) + left + right + (sym.len() * 2) - 1;
@@ -157,36 +193,57 @@ fn main() {
     // Spaces for width padding
     let w_pad = (0..w_pad_len).map(|_| " ").collect::<String>();
 
+    // Spaces for box moving
+    let mv_pad = (0..mv).map(|_| " ").collect::<String>();
+
     // Finished lines for height padding
     let spaces_len = longest + (w_pad_len * 2) + left + right;
     let spaces = (0..spaces_len).map(|_| " ").collect::<String>();
     let h_pad = (0..h_pad_len)
-        .map(|_| format!("{}{}{}{}\n", modded, sym, spaces, sym))
+        .map(|_| format!("{}{}{}{}{}\n", mv_pad, modded, sym, spaces, sym))
         .collect::<String>();
 
     // Spaces for left and right padding
     let l_pad = (0..left).map(|_| " ").collect::<String>();
     let r_pad = (0..right).map(|_| " ").collect::<String>();
 
+
+    //////////////////////////////////////////////////////////////////////////
+    // Title
+    //////////////////////////////////////////////////////////////////////////
+    let mut top = String::from("");
+    if title_len != 0 {
+        let beg = (0..width+1).map(|_| one_sym).collect::<String>();
+        let spaces_len = (width+1)-(title_len)-2*(sym.len());
+        let spaces = (0..(spaces_len/2)).map(|_| " ").collect::<String>();
+        if spaces_len % 2 == 0 {
+            top = format!("{}{}\n{}{}{}{}{}{}", mv_pad, beg, mv_pad, sym, spaces, title, spaces, sym);
+        } else {
+            top = format!("{}{}\n{}{}{}{}{} {}", mv_pad, beg, mv_pad, sym, spaces, title, spaces, sym);
+        }
+    }
+
     for h in 0..height {
         if h == 0 {
             let beg = (0..width).map(|_| one_sym).collect::<String>();
-            let filling = format!("{}\n{}", beg, h_pad);
+            let filling = format!("{}\n{}{}{}\n{}", top, mv_pad, delimiter, beg, h_pad);
             commentized.push_str(&filling);
         } else if h == height - 1 {
             let beg = (0..width).map(|_| one_sym).collect::<String>();
             let end;
             if cli.modded {
-                end = "/";
+                end = "/".to_string();
+            } else if cli.box_only {
+                end = one_sym.to_string();
             } else {
-                end = "*/";
+                end = "*/".to_string();
             }
-            let filling = format!("{}{}{}{}\n", h_pad, modded, beg, end);
+            let filling = format!("{}{}{}{}{}\n", h_pad, modded, mv_pad, beg, end);
             commentized.push_str(&filling);
         } else {
-            let beg = format!("{}{}{}{}", modded, sym, w_pad, l_pad);
+            let beg = format!("{}{}{}{}{}", mv_pad, modded, sym, w_pad, l_pad);
             let end = format!("{}{}{}\n", r_pad, w_pad, sym);
-            let len = longest - &lines[h - 1].len();
+            let len = longest - &lines[h - 1].chars().count(); // chars().count() for 2 bytes char
             let spaces = (0..len).map(|_| " ").collect::<String>();
             let filling = format!("{}{}{}{}", beg, &lines[h - 1], spaces, end);
             commentized.push_str(&filling);
